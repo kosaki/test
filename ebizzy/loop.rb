04e2e23b008de793f42ddd896b55534becac986c
KOSAKI Motohiro <kosaki.motohiro@gmail.com>
2014-03-21 17:55:27 -0400

    update loop.rb

--- ebizzy/loop.rb
+++ ebizzy/loop.rb
@@ -1,23 +1,62 @@
 sleep=5
 
-for i in [1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
+module Enumerable
+    def sum
+      self.inject(0){|accum, i| accum + i }
+    end
 
-  ENV["MALLOC_VRANGE"]=0.to_s
-  result = `./ebizzy -S #{sleep} -t #{i}`
-  ENV["MALLOC_VRANGE"]=1.to_s
-  result2 = `./ebizzy -S #{sleep} -t #{i}`
-  result2.gsub!("glibc: vrange enabled\n", "")
-  printf("%3d %8d %8d\n", i, result, result2);
+    def mean
+      self.sum/self.length.to_f
+    end
 
-#  ENV["MALLOC_VRANGE"]=1.to_s
-#  system("./ebizzy -S #{sleep} -t #{i}")
+    def sample_variance
+      m = self.mean
+      sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+      sum/(self.length - 1).to_f
+    end
+
+    def standard_deviation
+      return Math.sqrt(self.sample_variance)
+    end
+
 end
 
-#puts("---")
-#puts("vrange")
-#puts("---")
+for v in [0, 2]
+  ENV["MALLOC_VRANGE"] = v.to_s
+  printf("----- vrange = %d ---------------\n", v)
+  for i in [1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
+#    for i in [12]
+    results = []
 
+    9.times {|j|
+      ret = `./ebizzy -S #{sleep} -n 512 -t #{i}`
+      ret.gsub!(/glibc:.*\n/, "")
+      results[j] = ret.to_f
+      sleep 0.2
+    }
 
-#for i in [1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
-#  system("./ebizzy -S #{sleep} -t #{i}")
-#end
+    #drop min-max
+      results.sort!
+      results.shift(2)
+      results.pop(2)
+
+    avg = results.sum / results.size
+    std_per = results.standard_deviation / avg * 100
+    if std_per > 5
+      puts "   redo"
+      printf("   threads: %d\n", i)
+      printf("   data: %s\n", results.join(" "))
+      printf("   avg: %.2f\n", avg)
+      printf("   std: %.2f (%.2f%%)\n", results.standard_deviation, std_per)
+      redo;
+    end
+
+    printf("\n")
+    printf("threads: %d\n", i)
+    printf("data: %s\n", results.join(" "))
+    printf("avg: %.2f\n", avg)
+    printf("std: %.2f (%.2f%%)\n", results.standard_deviation, std_per)
+    printf("max: %.0f\n", results.max)
+    printf("min: %.0f\n", results.min)
+  end
+end
